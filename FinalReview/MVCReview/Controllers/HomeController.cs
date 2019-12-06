b139390bd6aa79bfc44b04efd4c772e5dee0be6d
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-12-06 15:48:49 -0600

    adding mvc project

---
+++ FinalReview/MVCReview/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MVCReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCReview.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About the University of Oklahoma Football program";
+
+            AboutClass a = new AboutClass();
+
+            return View(a);
+        }
+
+        
+    }
+}
