afa9275fd51580809b217a8259fb409d8716cf4d
Haley Leonard <Haley.M.Leonard-1@ou.edu>
2019-12-08 14:49:21 -0600

    following along video

---
+++ FinalReview/MVCReview/Controllers/ScheduleController.cs
@@ -0,0 +1,26 @@
+﻿using MVCReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCReview.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Schedule
+        public ActionResult Index(int? year)
+        {
+            if(year==null)
+            {
+                year = DateTime.Now.Year;
+            }
+            var games = db.FootballSchedules.Where(x=> x.Season==year.Value);
+
+            return View(games);
+        }
+    }
+}
